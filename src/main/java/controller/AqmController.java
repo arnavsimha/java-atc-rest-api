7689f451b3f4c031a83035769d7a8cceacfe6ac0
kwilsonO <kylewilson52@gmail.com>
2017-09-02 11:11:49 -0400

    added application class and controller base

--- src/main/java/controller/AqmController.java
+++ src/main/java/controller/AqmController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 public class AqmController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @RequestMapping("/healthz")
+    public String healthz() {
+        return counter.incrementAndGet() + " What are you a doctor?";
+    }
+
 }
